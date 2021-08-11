f131341602e1d69cb923168dee3e7bbd98419faa
KentaroFujimoto <KentaroFujimoto@users.noreply.github.com>
2021-08-11 15:28:45 +0900

    second commit

---
+++ lib/video_detail_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class VideoDetalPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        child: Center(
+            child: Text('動画')
+        ),
+      ),
+    );
+  }
+  
+}
